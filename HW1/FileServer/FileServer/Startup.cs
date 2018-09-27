0366d1c4a1c9238ced394e1d1c12ba8218c1b6fa
RootLyre <33261611+RootLyre@users.noreply.github.com>
2018-09-26 22:42:16 -0400

    Add files via upload

---
+++ HW1/FileServer/FileServer/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FileServer
+{
+    public class Startup
+    {
+      
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), @"Assets")),
+                RequestPath = new PathString("/Public/MyAssets")
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(context.Items["Mobile"].ToString());
+            });
+        }
+    }
+}
