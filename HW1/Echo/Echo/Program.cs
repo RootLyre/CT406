2d48a2afc4b7c553af55d109f9cb9156caf1bf67
RootLyre <33261611+RootLyre@users.noreply.github.com>
2018-09-26 21:40:39 -0400

    Add files via upload

---
+++ HW1/Echo/Echo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Ch02.Echo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseIISIntegration()
+                .Configure(app => {
+                    app.Run(async (context) => {
+                        var path = context.Request.Path;
+                        await context.Response.WriteAsync("<h1>" + path + "</h1>");
+                    });
+                })
+                .Build();
+            host.Run();
+        }
+    }
+}
+
