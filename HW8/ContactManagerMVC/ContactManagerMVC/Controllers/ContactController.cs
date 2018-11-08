fb9814899f1b54d2654e2cd617bd95b54ee34050
RootLyre <33261611+RootLyre@users.noreply.github.com>
2018-11-07 22:15:14 -0500

    Add files via upload

---
+++ HW8/ContactManagerMVC/ContactManagerMVC/Controllers/ContactController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ContactManagerMVC.Models;
+using ContactManagerMVC.Services;
+namespace ContactManagerMVC.Controllers
+{
+    public class ContactController : ControllerBase
+    {
+        private ContactRepository contactRepository;
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
+    }
+
+}
