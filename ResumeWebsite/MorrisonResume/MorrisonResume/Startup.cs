33e1dbf31773bc6d072ad234b5acc469bbd9a445
RootLyre <33261611+RootLyre@users.noreply.github.com>
2018-12-18 14:25:28 -0500

    Add files via upload

---
+++ ResumeWebsite/MorrisonResume/MorrisonResume/Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MorrisonResume.Models;
+using MorrisonResume.Data;
+
+namespace MorrisonResume
+{
+    public class Startup
+    {
+        //Creates a reference to the Context so I can initialize the Databases
+        //private readonly MorrisonResumeContext context;
+
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            //Add DbInitializer to Startup so that the database Exists
+           
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<MorrisonResumeContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MorrisonResumeContext")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            //Initialize Databases within MorrisonResumeContext
+            //DbInitializer.Initialize(context);
+            app.UseMvc();
+
+            
+        }
+    }
+}
